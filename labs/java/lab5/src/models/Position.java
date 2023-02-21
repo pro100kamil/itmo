0bcec344dd6482e5b733f6eca9663f5434a447d3
Kamil <74972827+pro100kamil@users.noreply.github.com>
2023-02-21 05:03:22 +0300

    Delete labs/java/lab5/src directory

--- labs/java/lab5/src/models/Position.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-public enum Position {
-    MANAGER,
-    HUMAN_RESOURCES,
-    HEAD_OF_DEPARTMENT,
-    LEAD_DEVELOPER,
-    BAKER;
-}
--- labs/java/lab5/src/models/Status.java
+++
@@ -1,8 +0,0 @@
-package models;
-
-public enum Status {
-    FIRED,
-    RECOMMENDED_FOR_PROMOTION,
-    REGULAR,
-    PROBATION;
-}
